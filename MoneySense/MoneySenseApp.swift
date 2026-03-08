2dfede33587f5d36ba3dea40e486be5f4ad9bbc6
jingkang97 <jingkang@lomotif.com>
2026-03-08 20:30:46 +0800

    Initial Commit

---
+++ MoneySense/MoneySenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneySenseApp.swift
+//  MoneySense
+//
+//  Created by Jing Kang Ng on 8/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MoneySenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
